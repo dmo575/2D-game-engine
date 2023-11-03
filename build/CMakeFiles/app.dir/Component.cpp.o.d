a257ce336e99d3b96d63c0d11c2102725cd3e67f
dmo575 <dmo5@protonmail.com>
2023-11-03 00:24:48 -0400

    Removed build files

--- build/CMakeFiles/app.dir/Component.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/app.dir/Component.cpp.o: \
- /home/dmo/projects/2d_engine/Component.cpp /usr/include/stdc-predef.h \
- /home/dmo/projects/2d_engine/Component.h \
- /home/dmo/projects/2d_engine/CoreStructs.h
--- build/CMakeFiles/app.dir/InputModule.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/app.dir/InputModule.cpp.o: \
- /home/dmo/projects/2d_engine/InputModule.cpp /usr/include/stdc-predef.h \
- /home/dmo/projects/2d_engine/InputModule.h \
- /home/dmo/projects/2d_engine/CoreStructs.h
